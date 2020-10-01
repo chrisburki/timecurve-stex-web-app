12b8d6f1f4391c24e482ab7b29ba02f27e18866b
Christoph Burki <chrisburki@gmx.ch>
2020-10-01 07:02:56 +0200

    New Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// Template driven Approach
+// import { NgForm } from '@angular/forms';
+
+import { FormControl, FormGroup } from '@angular/forms';
+
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'stex-app';
+export class AppComponent implements OnInit {
+
+  // Reactive driven Approach
+  searches: string[] = [];
+
+  searchForm: FormGroup;
+  searchField: FormControl;
+
+  ngOnInit() {
+    this.searchForm = new FormGroup({ 
+      searchField: new FormControl(null)
+    })
+    this.searchField = new FormControl();
+    this.searchField.valueChanges
+      .pipe(
+        debounceTime(400),
+        distinctUntilChanged()
+      )
+      .subscribe(term => {
+        this.searches.push(term)
+      });
+  }
+
+  // Template driven Approach  
+  // ngOnInit() { }
+
+  //   onSubmit(form: NgForm) {
+  //    console.log(form);
+  //  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { CommonModule } from '@angular/common';
+// import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+//    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
